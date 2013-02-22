fa32b979e846ab7b741d5a31d8ea58c26a32346e
Nathan B <threefirstnames89@gmail.com>
2013-02-22 09:29:15 -0800

    Re-created repo
    Basic app working. Got devies authentication working.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3FullcalendarDemo::Application.config.secret_token = 'fe1c0254177b89b2172b62b76298be59ad1c05291153254ffd546996c3b4ddf6406137d73fdc190eaafae75daebb099649bd16faf5e02b419490b8d04d9f3f4e'
